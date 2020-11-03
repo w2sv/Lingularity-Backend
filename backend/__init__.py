cb412cd97923cc2d60054adfdd9c1857bb56d33a
W2SV <zangenbergjanek@googlemail.com>
2020-11-03 15:51:39 +0100

    access paths via pathlib.Path

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,9 @@
-import os
 import logging
+from pathlib import Path
 from .version import __version__
 
 
-_BACKEND_DATA_PATH = f'{os.getcwd()}/backend/data'
+_BACKEND_DATA_PATH = f'{Path(__file__).parent}/backend/data'
 
 SENTENCE_DATA_PATH = f'{_BACKEND_DATA_PATH}/sentence-data'
 TOKEN_MAPS_PATH = f'{_BACKEND_DATA_PATH}/token-maps'
